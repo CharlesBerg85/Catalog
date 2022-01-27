a4126758a57ed5b0e9c450a568b1bc93f3cb00a5
Charles M Berg <77517285+CharlesBerg85@users.noreply.github.com>
2022-01-26 23:47:54 -0800

    Added Repository, Entities, and Contoller

---
+++ Controllers/ItemsController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.Controllers
+{
+    // GET /items
+
+    [ApiController]
+    [Route("[items]")]
+
+    public class ItemsController : ControllerBase
+    {
+
+    }
+}
