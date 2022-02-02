5c4db208ad3e5e94224efb556c97ab7255516814
Charles M Berg <77517285+CharlesBerg85@users.noreply.github.com>
2022-02-01 17:57:32 -0800

    Hooked up MongoDB along with docker, Create Item route working

--- Repositories/MongoDbItemsRepository.cs
+++ Repositories/MongoDbItemsRepository.cs
@@ -1,31 +1,41 @@
 using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using Catalog.Entities;
+using MongoDB.Bson;
+using MongoDB.Driver;
 
 namespace Catalog.Repositories
 {
     public class MongoDbItemsRepository : IItemsRepository
     {
-        public void CreateItem(Item item)
+        private const string databaseName = "catalog";
+        private const string collectionName = "items";
+        private readonly IMongoCollection<Item> itemsCollection;
+        private readonly FilterDefinitionBuilder<Item> filterBuilder = Builders<Item>.Filter;
+
+        public MongoDbItemsRepository(IMongoClient mongoClient)
         {
-            throw new NotImplementedException();
+            IMongoDatabase database = mongoClient.GetDatabase(databaseName);
+            itemsCollection = database.GetCollection<Item>(collectionName);
         }
 
+        public void CreateItem(Item item)
+        {
+            itemsCollection.InsertOne(item);
+        }
         public void DeleteItem(Guid id)
         {
             throw new NotImplementedException();
         }
-
         public Item GetItem(Guid id)
         {
             throw new NotImplementedException();
         }
-
         public IEnumerable<Item> GetItems()
         {
             throw new NotImplementedException();
         }
-
         public void UpdateItem(Item item)
         {
             throw new NotImplementedException();
--- Startup.cs
+++ Startup.cs
@@ -16,6 +16,12 @@ using System.Text.Json;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Http;
 using Catalog.Repositories;
+using MongoDB.Driver;
+using Catalog.Settings;
+using MongoDB.Bson.Serialization.Serializers;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson;
+
 namespace Catalog
 {
     public class Startup
@@ -30,7 +36,17 @@ namespace Catalog
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddSingleton<IItemsRepository, InMemItemsRepository>();
+            BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String));
+            BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(BsonType.String));
+
+            services.AddSingleton<IMongoClient>(ServiceProvider =>
+            {
+                var settings = Configuration.GetSection(nameof(MongoDbSettings)).Get<MongoDbSettings>();
+                return new MongoClient(settings.ConnectionString);
+            });
+
+            services.AddSingleton<IItemsRepository, MongoDbItemsRepository>();
+
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
